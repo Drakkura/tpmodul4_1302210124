554f12499e90add0ea0ceeac5c4ef745f0790560
Drakkura <gonhgyhjj1@gmail.com>
2023-03-12 05:47:51 +0700

    Menambahkan implementasi dengan table-driven

--- tpmodul4_1302210124/Program.cs
+++ tpmodul4_1302210124/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace mod4_1302210124 {
+    internal class program {
+        public enum dataKota {
+            Batununggal,
+            Kujangsari,
+            Mengger,
+            Wates,
+            Cijaura,
+            Jatisari,
+            Margasari,
+            Sekejati,
+            Kebonwaru,
+            Maleer,
+            Samoja
+        };
+        public class getKodePos {
+            public static int getKode(dataKota kota) {
+                int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+                return kodepos[(int)kota];
+            }
+        }
+        public static void Main(string[] args) {
+            dataKota kota = dataKota.Jatisari;
+            int kode = getKodePos.getKode(kota);
+            Console.WriteLine("Kelurahan: " + kota + "\nkodepos: " + kode);
+        }
+    }
+}
